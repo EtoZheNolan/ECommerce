98e186894ff976fe41d5ac8962937cd6acf49cc0
Evgeny Prytkov <>
2024-02-14 09:57:49 +0300

    Init

---
+++ ECommerce.UserService/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.UserService.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class UserController : ControllerBase
+{
+    
+    [Route("sign-up")]
+    public async Task<IActionResult> SignUp()
+    {
+        return Ok();
+    }
+}
